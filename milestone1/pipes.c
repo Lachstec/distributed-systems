491636e69116d35cd0f2401a136e8b6281c897d2
János <janos.euler@gmail.com>
2023-11-13 18:46:55 +0100

    Fixed Uglies, Explained stuff

--- milestone1/pipes.c
+++ milestone1/pipes.c
@@ -6,22 +6,29 @@
 char wbuffer[MAX_STRING_SIZE];
 char rbuffer[MAX_STRING_SIZE];
 
+//Process Commmunication with Pipes. Input Process writes while Output Process reads
+//This is a unidirectional communication.
+
 void input(int array[]);
 
 void output(int array[]);
 
 int main(int argc, char **argv)
 {   
+    printf("Communication test(exit with CTRL + c)\n");
     int pipefds[2];
     int returnstatus;
     int pid;
+    //This function creates a pipe [0] is the reading end while [1] is the writing end
+    //If something goes wrong during creation, the programm will exit with statuscode (1).
     returnstatus=pipe(pipefds);
     if(returnstatus==-1){
-        printf("Unable to create pipe\n");
+        fprintf(stderr, "Unable to create pipe\n");
         return 1;
     }
+    //Process is forked to create two communicating processes. 
+    //The if determines which function should be called.
     pid = fork();
-    printf("Communication test(exit with CTRL + c)\n");
     while (1)
     {
         if(pid==0){
@@ -39,10 +46,11 @@ void input(int pipe[])
     memset(wbuffer, 0, MAX_STRING_SIZE);
     fgets(wbuffer, MAX_STRING_SIZE, stdin);
     write(pipe[1], wbuffer, sizeof(wbuffer));
+    sleep(1);
 }
 
 void output(int pipe[])
 {
     read(pipe[0],rbuffer,sizeof(rbuffer));
-    printf("Entered Text: %s\n", rbuffer);
+    printf("Entered Text: %s", rbuffer);
 }
