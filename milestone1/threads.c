ff5eda0c644ccc09932cae83b207869c753f7063
Max Cherris <MCherris@protonmail.com>
2023-11-28 16:31:32 +0100

    Updated Threads, made em Beautyful

--- milestone1/threads.c
+++ milestone1/threads.c
@@ -8,7 +8,6 @@
 #define MAX_STRING_SIZE 128
 char buffer[MAX_STRING_SIZE];
 bool input_ready = false;
-pthread_mutex_t mutex;
 
 void* input() {
     while(1) {
@@ -31,19 +30,11 @@ void output() {
 
 int main (int argc, char** argv) {
     pthread_t threadIn;
-    //threadOut;
-
-
     printf("Communication test (exit with CTRL+c");
 
     pthread_create(&threadIn, NULL, input, NULL);
-    //pthread_create(&threadOut, NULL, output, NULL);
     while(1) {
         output();
-    }//pthread_join(threadIn, NULL);
-    //pthread_join(threadOut, NULL);
-
-
-
+    }
     return 0;
 }
