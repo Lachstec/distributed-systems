efa6bd85c527ff4397f53a8805afe80977c95516
leonlux <leonlux@icloud.com>
2023-11-28 10:46:32 +0100

    add some explanatory comments

--- milestone1/sockets.c
+++ milestone1/sockets.c
@@ -11,6 +11,8 @@
 #define MAX_STRING_SIZE 128
 char buffer[MAX_STRING_SIZE];
 
+// input takes a socket file descriptor and uses it to read in a string from the command line
+// and write it to the socket. It then sleeps for 100ms.
 void input(int socket_fd) {
     	printf("Enter Text: ");
     	memset(buffer, 0, MAX_STRING_SIZE);
@@ -21,17 +23,22 @@ void input(int socket_fd) {
 	usleep(100);
 }
 
+// output takes a socket file descriptor and reads from it to print it to the console.
 void output(int socket_fd) {
 	read(socket_fd, buffer, sizeof(buffer));
 	printf("Text entered: %s\n", buffer);
 }
 
 int main(int argc, char **argv) {
+	// array to store the socket file descriptors
 	int socket_vec[2];
+	// use the socketpair to create two sockets and store them into socket_vec.
+	// if an error occurs, it gets printed and the program returns EXIT_FAILURE.
 	if(socketpair(AF_UNIX, SOCK_STREAM, 0, socket_vec) != 0) {
 		fprintf(stderr, "error creating socket pair: %s\n", strerror(errno));
 		return EXIT_FAILURE;
 	}
+	// fork and use on process for input and the other for output.
 	pid_t pid = fork();
 	if(pid == 0) {
 		printf("input process has pid %d\n", getpid());
